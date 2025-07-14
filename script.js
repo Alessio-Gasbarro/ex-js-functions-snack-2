e378434a689709e9ff112d2563631bd77e7fee90
Alessio-Gasbarro <gasbarro.developer@gmail.com>
2025-07-14 19:26:41 +0200

    [UPD] Snack 2 DONE

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-//SNACK 1
-//Funzione dichiarativa
+// 🏆 Snack 1 - Funzioni di somma
+// 1. Funzione dichiarativa
 function somma(a, b) {
     return a + b;
 }
 
-//Funzione anonima assegnata a una variabile
+// 2. Funzione anonima assegnata a una variabile
 const sommaAnonima = function (a, b) {
     return a + b;
-}
+};
 
-//Funzione freccia (Arrow Function)
+// 3. Arrow function
 const sommaArrow = (a, b) => a + b;
+
+// 🏆 Snack 2 - Arrow function che calcola il quadrato di un numero
+const quadrato = n => n * n;
