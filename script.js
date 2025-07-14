f23e965b53dc41e5fb673652c52f17e4f1e67630
Alessio-Gasbarro <gasbarro.developer@gmail.com>
2025-07-14 19:27:23 +0200

    [UPD] Snack 3 DONE

--- script.js
+++ script.js
@@ -14,3 +14,12 @@ const sommaArrow = (a, b) => a + b;
 
 // 🏆 Snack 2 - Arrow function che calcola il quadrato di un numero
 const quadrato = n => n * n;
+
+// 🏆 Snack 3 - Funzione eseguiOperazione con callback
+const sommaCB = (a, b) => a + b;
+const moltiplica = (a, b) => a * b;
+
+const eseguiOperazione = (a, b, operazione) => operazione(a, b);
+
+console.log("Snack 3:", eseguiOperazione(3, 4, sommaCB)); // Output: 7
+console.log("Snack 3:", eseguiOperazione(3, 4, moltiplica)); // Output: 12
