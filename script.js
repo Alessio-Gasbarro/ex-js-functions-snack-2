01e54546b24f3799290d14648b7077ef5c9f231a
Alessio-Gasbarro <gasbarro.developer@gmail.com>
2025-07-14 19:22:36 +0200

    [UPD] Snack 1 DONE

---
+++ script.js
@@ -0,0 +1,13 @@
+//SNACK 1
+//Funzione dichiarativa
+function somma(a, b) {
+    return a + b;
+}
+
+//Funzione anonima assegnata a una variabile
+const sommaAnonima = function (a, b) {
+    return a + b;
+}
+
+//Funzione freccia (Arrow Function)
+const sommaArrow = (a, b) => a + b;
